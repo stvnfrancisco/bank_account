115a0222797faa71391f1d404ef823883c40ee5c
steven francisco <stvnfrancisco@yahoo.com>
2015-08-01 16:44:49 -0700

    edits css... change background color and centers all text

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var BankAccount = {
-  balance: 0,
+  balance: 0.0,
 
   withdraw: function(amount) {
     this.balance = this.balance - amount;
@@ -18,13 +18,14 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#first-name").val();
     var inputtedLastName = $("input#last-name").val();
     var initialBalance = parseInt($("input#initial").val());
-    isNaN(initialBalance) ? initialBalance = 0 : initialBalance = initialBalance;
+    isNaN(initialBalance) ? initialBalance = 0.0 : initialBalance = initialBalance;
     newBankAccount.owner = inputtedFirstName + " " + inputtedLastName;
     newBankAccount.deposit(initialBalance);
-
-    $("input#first-name").val("");
-    $("input#last-name").val("");
-    $("input#initial").val("");
+    var clearFields = function() {
+      $("input#first-name").val("");
+      $("input#last-name").val("");
+      $("input#initial").val("");
+    }
 
     $("#banking").show();
     $("#status").show();
@@ -39,8 +40,8 @@ $(document).ready(function() {
   $("form#depositwithdraw").submit(function(event){
     var depositAmount = parseInt($("input#deposit").val());
     var withdrawalAmount = parseInt($("input#withdraw").val());
-    isNaN(depositAmount) ? depositAmount = 0 : depositAmount = depositAmount;
-    isNaN(withdrawalAmount) ? withdrawalAmount = 0 : withdrawalAmount = withdrawalAmount;
+    isNaN(depositAmount) ? depositAmount = 0.0 : depositAmount = depositAmount;
+    isNaN(withdrawalAmount) ? withdrawalAmount = 0.0: withdrawalAmount = withdrawalAmount;
 
     newBankAccount.withdraw(withdrawalAmount);
     newBankAccount.deposit(depositAmount);
